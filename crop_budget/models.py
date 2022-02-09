704bd52c18bd8e46b6f1af0b22e135b6a12b3477
Vaitheeswaran <mpvaitheeswaran@gmail.com>
2022-02-09 20:25:49 +0530

    created model.

--- crop_budget/models.py
+++ crop_budget/models.py
@@ -1,3 +1,21 @@
+from datetime import date
+from unicodedata import name
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Crop(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    crop_name = models.CharField(max_length=15)
+    total_expense = models.FloatField(default=0,blank=True)
+    crop_value = models.FloatField(default=0,blank=True)
+    date = models.DateTimeField(auto_now=False)
+    def __str__(self) -> str:
+        return f'{self.crop_name}'
+
+class Expense(models.Model):
+    crop = models.ForeignKey(Crop,on_delete=models.CASCADE)
+    name = models.CharField(max_length=15)
+    cost = models.FloatField(default=0)
+    date = models.DateTimeField(auto_now=True)
+    def __str__(self) -> str:
+        return f'{self.name}'
--- crop_budget/urls.py
+++ crop_budget/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path,include
+from django.urls import path
 from crop_budget import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('',views.home,name='home'),
+    path('accounts/login',
+    auth_views.LoginView.as_view(template_name='crop_budget/login.html'
+        ,redirect_authenticated_user=True),
+    name='login'),
 ]
